4c0512470decb4c4d7e02c64f05c0994e258d338
Vadim Konshin <vdmkonshin@gmail.com>
2024-03-03 10:47:45 +0200

    refactoring code

--- src/classes.py
+++ src/classes.py
@@ -8,7 +8,7 @@ class Category:
     category_count = 0
     product_count = 0
 
-    def __init__(self, name: str, description: str, products: list):
+    def __init__(self, name: str, description: str, products: list = None) -> None:
         self.name = name
         self.description = description
         self.products = products
